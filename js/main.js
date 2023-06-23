24801a51c4a530c1798ec6c46dbd3315d32d8cf0
neverson42 <35622011+neverson42@users.noreply.github.com>
2023-06-22 21:43:52 -0500

    add howto

--- js/main.js
+++ js/main.js
@@ -128,3 +128,37 @@ function fcatvis(elem) {
 		document.getElementById('noclicklink').remove();
 		document.getElementById('hiddencosts').style.display="block";
 	}
+	
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  }
+  
+  function tutorial() {
+	  document.getElementById('tutorial').style.display='block';
+	  document.getElementById('main').style.display='none';
+  }
+  
+  function clstut() {
+	  document.getElementById('tutorial').style.display='none';
+	  document.getElementById('main').style.display='block';
+  }
